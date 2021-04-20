9a33dd2f9bc902f8a178277fec289f79ff760d0b
aonnerst <anna.onnerstad@cern.ch>
2021-04-20 11:40:17 +0300

    fixed harmonic index

--- macros/FitTwoPart.C
+++ macros/FitTwoPart.C
@@ -41,23 +41,24 @@ void LoadData(TString inputname)
 	TFile *fIn = TFile::Open(inputname,"read");
 	for(int ic=0; ic<NC; ic++){
 		hDeltaPhiSum[ic]=(TH1D*)fIn->Get(Form("hDeltaPhiSum_C%02d",ic));
-
+		hDeltaPhiSum[ic]->Rebin(2);
 	}
 
 }
 
 void FitDrawTwo(int ic=0)
 {
+	cout<<"FitDrawTwo " << ic <<endl;
 	TString ParName[NH+1]={"const","v_1","v_2","v_3","v_4","v_5","v_6","v_7"};
 	TString strformula = "[0]*(1";
-	for (Int_t ih=0; ih<NH; ih++){
-		strformula += Form("+2*TMath::Power([%d],2)*TMath::Cos(%d*x)",ih+1,ih+1);
+	for (Int_t ih=1; ih<=NH; ih++){
+		strformula += Form("+2*TMath::Power([%d],2)*TMath::Cos(%d*x)",ih,ih);
 	}
 	strformula+=")";
 	TF1 *fFit = new TF1("fFit", strformula, 0, 2.0*TMath::Pi());
 	fFit->SetParameter(0,1E4);
-	for (Int_t i=1; i<NH; i++) fFit->SetParameter(i,0.06);
-	for (Int_t i=0; i<NH+1; i++) fFit->SetParName(i,ParName[i]);
+	for (Int_t i=1; i<=NH; i++) fFit->SetParameter(i,0.06);
+	for (Int_t i=0; i<=NH+1; i++) fFit->SetParName(i,ParName[i]);
 	TF1 *fFitvn[NH];
 
 	gStyle->SetOptStat(0);
@@ -69,7 +70,7 @@ void FitDrawTwo(int ic=0)
 
 	double lowx = 0.,highx=2*TMath::Pi();
   	double ly=hDeltaPhiSum[ic]->GetMinimum()*0.99,hy=hDeltaPhiSum[ic]->GetMaximum()*1.01;
-  	TH2F *hfr = new TH2F("hfr",Form("Cent%02d",ic), 100,lowx, highx, 10, ly, hy); // numbers: tics x, low limit x, upper limit x, tics y, low limit y, upper limit y
+  	TH2F *hfr = new TH2F("hfr"," ", 100,lowx, highx, 10, ly, hy); // numbers: tics x, low limit x, upper limit x, tics y, low limit y, upper limit y
   	hset( *hfr, "#Delta#phi=#phi_{1}-#phi_{2}", "dN/d#Delta#phi",0.9,0.9, 0.05,0.05, 0.01,0.01, 0.03,0.03, 510,505);//settings of the upper pad: x-axis, y-axis
   	hfr->Draw();
   	
@@ -79,25 +80,27 @@ void FitDrawTwo(int ic=0)
 	fFit->Draw("same");
 
     
-    for (Int_t n=0; n<=(NH-1); n++){
-    	TString formula = Form("[0]*(1 + 2*TMath::Power([1],2)*TMath::Cos(%d*x))",n+1);
-		fFitvn[n]= new TF1(Form("fFitvn%02d",n+1),formula, 0, 2.0*TMath::Pi());//Just for drawing
+    for (Int_t i=1; i<=NH; i++){
+    	TString formula = Form("[0]*(1 + 2*TMath::Power([1],2)*TMath::Cos(%d*x))",i);
+		fFitvn[i]= new TF1(Form("fFitvn%02d",i),formula, 0, 2.0*TMath::Pi());//Just for drawing
 	}
 
 	//get vn's from fit
-
-    TLegend *legendPhi = new TLegend(0.55,0.7,0.75,0.9,"","brNDC");
+	cout << "ok, 2!" <<endl;
+    TLegend *legendPhi = new TLegend(0.6,0.6,0.8,0.85,"","brNDC");
     legendPhi->SetTextSize(0.03);legendPhi->SetBorderSize(0);legendPhi->SetFillStyle(0);//legend settings;
+	legendPhi->AddEntry(fFitvn[1],Form("Centrality %s",strCentrality[ic].Data()));
 	//for loop 
-	for (Int_t n=1; n<=(NH-1); n++) {
-		vn[n][ic]=fFit->GetParameter(n+1);
-		vnError[n][ic]=fFit->GetParError(n+1);
-		fFitvn[n]->SetParameter(1,vn[n][ic]);// Setting individual component vn
-		fFitvn[n]->SetParameter(0, fFit->GetParameter(0));//Normalization
-		fFitvn[n]->SetLineColor(gColors[n+1]);
-		fFitvn[n]->Draw("same");
-		legendPhi->AddEntry(fFitvn[n],Form("n = %d, v_n = %.3f #pm %.4f ",n+1, vn[n][ic], vnError[n][ic]),"l");
+	for (Int_t i=2; i<=NH; i++) {
+		vn[i][ic]=fFit->GetParameter(i);
+		vnError[i][ic]=fFit->GetParError(i);
+		fFitvn[i]->SetParameter(1,vn[i][ic]);// Setting individual component vn
+		fFitvn[i]->SetParameter(0, fFit->GetParameter(0));//Normalization
+		fFitvn[i]->SetLineColor(gColors[i]);
+		fFitvn[i]->Draw("same");
+		legendPhi->AddEntry(fFitvn[i],Form("v_{%d} = %.3f #pm %.4f ",i, vn[i][ic], vnError[i][ic]),"l");
 	}	
+	cout << " ok 3!" << endl;
 	legendPhi->Draw();
 	gPad->GetCanvas()->SaveAs(Form("../figs/TwoPartDecomposeC%02d.pdf",ic));
 
@@ -108,9 +111,9 @@ void SaveVns()
 	double Cent[NC] = {0.,1.,2.};
 	double eCent[NC] = {0.,0.,0.};
 	TGraphErrors *gr_fittwo[NH];
-	for(int ih=1; ih<NH; ih++)gr_fittwo[ih]= new TGraphErrors(NC,Cent,vn[ih],eCent,vnError[ih]);
+	for(int ih=1; ih<=NH; ih++)gr_fittwo[ih]= new TGraphErrors(NC,Cent,vn[ih],eCent,vnError[ih]);
 	TFile *output = new TFile("out_VnFitTwo.root","recreate");
-	for(int ih=1; ih<NH; ih++)gr_fittwo[ih]->Write(Form("gr_fittwo_H%02d_cent",ih));
+	for(int ih=1; ih<=NH; ih++)gr_fittwo[ih]->Write(Form("gr_fittwo_H%02d_cent",ih));
 	output->Write();
 	output->Close();
 }
